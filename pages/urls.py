28e1be146ca2ae48c3962d7559406dfbc5541bd1
piyush-jhawar <piyushjhawar24@gmail.com>
2022-07-29 20:19:14 -0500

    Completed setting the Pages for Nav header and adding styling

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from pages import views
 
 urlpatterns = [
     path("", view=views.home, name="home"),
+    path('about/', view=views.about, name='about'),
+    path('services/', view=views.services, name='services'),
+    path('contact/', view=views.contact, name='contact'),
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request=request, template_name="pages/home.html", context={})
+
+
+def about(request):
+    return render(request=request, template_name="pages/about.html", context={})
+
+
+def services(request):
+    return render(request=request, template_name="pages/services.html", context={})
+
+def contact(request):
+    return render(request=request, template_name="pages/contact.html", context={})
