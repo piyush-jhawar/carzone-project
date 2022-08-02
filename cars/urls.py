154bd3adb096c751c037b6e02e22198bb98715ef
piyush-jhawar <piyushjhawar24@gmail.com>
2022-08-01 23:04:36 -0500

    Cars, Car detail and pagination done.

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,6 @@ from cars import views
 
 urlpatterns = [
     path("", view=views.cars, name="cars"),
+    path("<int:id>/", view=views.car_detail, name='car_detail'),
 
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, Page, PageNotAnInteger
+
+from cars.models import Car
 
 # Create your views here.
 
 
 def cars(request):
-    return render(request=request, template_name='cars/cars.html', context={})
+    cars = Car.objects.order_by("-created_date").all()
+    paginator = Paginator(cars, 1)
+    page = request.GET.get("page")
+    paged_cars = paginator.get_page(page)
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", paged_cars)
+    return render(request=request, template_name='cars/cars.html', context={"cars": paged_cars})
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, id=id)
+    return render(request=request, template_name="cars/car_detail.html", context={'single_car': single_car})
+    
