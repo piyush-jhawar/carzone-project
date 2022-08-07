3a088fd727e6073e2f8c44424852cd44d26593cd
piyush-jhawar <piyushjhawar24@gmail.com>
2022-08-07 15:04:28 -0500

    Implemented Password hide unhide toggle.

--- carzone/static/js/app.js
+++ carzone/static/js/app.js
@@ -484,3 +484,59 @@ $(function () {
 setTimeout(function(){
     $('#message').fadeOut('slow');
 }, 4000)
+
+// const togglePassword1 = document.querySelector('#togglePassword1');
+//   const password1 = document.querySelector('#password1');
+
+//   togglePassword1.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+//     password1.setAttribute('type', type);
+//     // toggle the eye slash icon
+//     this.classList.toggle('fa-eye-slash');
+// });
+
+// const togglePassword2 = document.querySelector('#togglePassword2');
+//   const password2 = document.querySelector('#password2');
+
+//   togglePassword2.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+//     password2.setAttribute('type', type);
+//     // toggle the eye slash icon
+//     this.classList.toggle('fa-eye-slash');
+// });
+
+const togglePassword1 = document.querySelector('#togglePassword1');
+const password1 = document.querySelector('#password1');
+
+togglePassword1.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+    password1.setAttribute('type', type);
+    if (type === "text") {
+        this.classList.add("fa-eye");
+        this.classList.remove("fa-eye-slash")
+
+    }else {
+        this.classList.add("fa-eye-slash");
+        this.classList.remove("fa-eye")
+    }
+});
+
+const togglePassword2 = document.querySelector('#togglePassword2');
+const password2 = document.querySelector('#password2');
+
+togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password';
+    password2.setAttribute('type', type);
+    if (type === "text") {
+        this.classList.add("fa-eye");
+        this.classList.remove("fa-eye-slash")
+
+    }else {
+        this.classList.add("fa-eye-slash");
+        this.classList.remove("fa-eye")
+    }
+});
